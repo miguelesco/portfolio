268787e233a4b4195015e52ad99d361d5e1ebd5e
miguelesco <miuelangel1999@gmail.com>
2021-06-29 12:48:06 -0400

    generation of cards dinamic

--- scripts.js
+++ scripts.js
@@ -2,6 +2,55 @@ const sidebar = document.getElementById('sideBar');
 const hamburguerIcon = document.getElementById('hamburguerIcon');
 const closeBtn = document.getElementsByClassName('close-sidebar')[0];
 const navigationTag = document.getElementsByClassName('sidebarNavigation');
+const projectSection = document.getElementById('works');
+
+
+const cardChild = [];
+const content = [
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageURL: '',
+    sourceURL: '',
+    liveURL: '',
+    categories: ['Ruby on rails', 'css', 'Javascript', 'HTML'],
+    action: ''
+  }
+];
+
+const createElement = (elementType = '' , elementContent , elementClass = '',  elementId = '') => {
+  const newElement = document.createElement(elementType);
+  newElement.innerHTML = elementContent;
+  if(elementClass) newElement.className = elementClass;
+  if(elementId) newElement.id = elementId;
+  return newElement;
+}
+
+const renderCardContent = () => {
+  content.forEach((cardContent, i) => {
+    const {title, description, imageURL, sourceURL, liveURL, categories, action} = cardContent;
+
+
+    cardChild.push({
+      cardImage: createElement('div', `<img src=${imageURL} alt=${title}>`, 'projectImage', '' ),
+      cardTitle: createElement('article', `<h3>${title}</h3>`, 'cardTitle', ''),
+      categories: createElement('ul', '', 'projectCategories', ''),
+      buttonContainer: createElement('div', '', 'seeProjectContainer', '')
+    });
+    categories.forEach(categorie => cardChild[i].categories.append(createElement('li', categorie, '', '')));
+    const button = createElement('button', 'See Project', 'seeProjectBtn', '');
+    button.addEventListener('click', () => {console.log('click button')});
+    cardChild[i].buttonContainer.append(button);
+  });
+  console.log(cardChild);
+
+  cardChild.forEach(child => {
+    const {cardImage, cardTitle, categories, buttonContainer} = child;
+    card.append(cardImage, cardTitle, categories, buttonContainer);
+  });
+
+  cardContainer.appendChild(card);
+}
 
 const displaySidebar = () => {
   sidebar.style.display = 'block';
@@ -10,6 +59,19 @@ const displaySidebar = () => {
 const closeSideBar = () => {
   sidebar.style.display = 'none';
 };
+
+const seeProject = () => {
+  //open popup
+};
+
+
+
+const sectionTitle = createElement('h2', 'My Recent Works ', 'blueTitleText', '');
+const divider = createElement('div', '', 'divider', '');
+const card = createElement('li', '', 'project-card', '');
+const cardContainer = createElement('ul', '', 'grid-card-container', '' );
+
+renderCardContent();
 
 hamburguerIcon.addEventListener('click', displaySidebar);
 closeBtn.addEventListener('click', closeSideBar);
@@ -22,3 +84,9 @@ window.onclick = function window(event) {
     sidebar.style.display = 'none';
   }
 };
+
+projectSection.append(
+  sectionTitle, 
+  divider,
+  cardContainer
+);
