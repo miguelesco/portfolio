079b169a04d075f68ba2e4c37f4463d736985b5c
miguelesco <miuelangel1999@gmail.com>
2021-06-30 14:00:52 -0400

    Fix linters errors

--- modules/formValidation.js
+++ modules/formValidation.js
@@ -1,42 +1,41 @@
-function FormValidation () {
+function FormValidation() {
   const form = document.getElementById('contactMe');
   const email = document.getElementById('email');
   const emailError = document.querySelector('#email + span.error');
   const textArea = document.getElementById('textArea');
 
+  function showError() {
+    email.style.marginBottom = '0px';
+    textArea.style.marginTop = '25px';
+    if (email.validity.valueMissing) {
+      emailError.textContent = 'You need to enter an e-mail address.';
+    } else if (email.validity.typeMismatch) {
+      emailError.textContent = 'Entered value needs to be an e-mail address.';
+    } else if (email.validity.tooShort) {
+      emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    } else if (email.value !== email.value.toLowerCase()) {
+      emailError.textContent = 'The email has to be all in lower case';
+    }
+
+    emailError.className = 'error active';
+  }
+
   const validateEmail = () => {
-    if(email.validity.valid && email.value === email.value.toLowerCase()) {
-      //display error
+    if (email.validity.valid && email.value === email.value.toLowerCase()) {
+      // display error
       emailError.textContent = '';
       emailError.className = 'error';
-      
-    }else {
+    } else {
       showError();
     }
   };
 
   const onSubmit = (event) => {
-    if(!email.validity.valid || email.value !== email.value.toLowerCase()) {
+    if (!email.validity.valid || email.value !== email.value.toLowerCase()) {
       showError();
       event.preventDefault();
     }
   };
-
-  function showError() {
-    email.style.marginBottom = '0px';
-    textArea.style.marginTop = '25px';
-    if(email.validity.valueMissing) {
-      emailError.textContent = 'You need to enter an e-mail address.';
-    } else if(email.validity.typeMismatch) {
-      emailError.textContent = 'Entered value needs to be an e-mail address.';
-    } else if(email.validity.tooShort) {
-      emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
-    }else if(email.value !== email.value.toLowerCase()){
-      emailError.textContent = 'The email has to be all in lower case';
-    }
-  
-    emailError.className = 'error active';
-  }
 
   email.addEventListener('input', () => validateEmail());
   form.addEventListener('submit', (event) => onSubmit(event));
